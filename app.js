637c0b711093d6b374117414a7b636108d16d3f1
Anu <annuv231@gmail.com>
2022-08-25 20:27:09 +0530

    added Mongodb database

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 require("dotenv").config({ path: "./config/.env" });
+const connectDatabase = require("./config/db");
 PORT = process.env.PORT;
 const truth = require("./route/truth");
+connectDatabase();
 app.use("/api/v1", truth);
 app.listen(PORT, () =>
   console.log(
