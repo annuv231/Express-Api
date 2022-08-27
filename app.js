44449bfca9e0d0659b5765f1199f22df515d16e9
Anu <annuv231@gmail.com>
2022-08-27 17:25:08 +0530

    ssetting up middleware

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ require("dotenv").config({ path: "./config/.env" });
 const connectDatabase = require("./config/db");
 PORT = process.env.PORT;
 const truth = require("./route/truth");
+
+//conoecting mongoDB database
 connectDatabase();
+
+//getting data
 app.use("/api/v1", truth);
+
+//setting up middleware
+
+const middlware = (req, res, next) => {
+  console.log("this is sending from middlware");
+};
+app.use(middlware);
+//liesting to the server
 app.listen(PORT, () =>
   console.log(
     `server started at port ${PORT} in ${process.env.NODE_ENV} server `
--- route/truth.js
+++ route/truth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
+
+//getting data
 router.get("/truth", (req, res) => {
   res.json({ message: "when did you sleep last time" });
 });
