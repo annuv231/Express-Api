6eef1c5229264ebe483c812e73bc242598984314
Anu <annuv231@gmail.com>
2022-11-03 09:32:38 +0530

    added a comment

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ require("dotenv").config({ path: "./config/.env" });
 const connectDatabase = require("./config/db");
 PORT = process.env.PORT;
 const truth = require("./route/truth");
-
 //conoecting mongoDB database
 connectDatabase();
 
 //setting up middleware
-
 app.use("/api/v1/truth", (req, res, next) => {
   console.log("Request type:", req.method);
   req.Method = req.method;
